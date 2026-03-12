661f6173211234de1b10fc3721e5af73f5f9b22d
narayananvenkatesh05-jr <narayananvenkatesh05@gmail.com>
2026-03-12 09:28:20 +0530

    UC2: Basic room types and static availability

--- src/BookMyStayApp.java
+++ src/BookMyStayApp.java
@@ -2,10 +2,20 @@ public class BookMyStayApp {
 
     public static void main(String[] args) {
 
-        System.out.println("=================================");
-        System.out.println("     WELCOME TO BOOK MY STAY     ");
-        System.out.println("=================================");
-        System.out.println("Hotel Booking System Started");
+        System.out.println("WELCOME TO BOOK MY STAY");
+
+        showRoomTypes();
 
     }
+
+    public static void showRoomTypes() {
+
+        System.out.println("Available Room Types:");
+
+        System.out.println("1. Standard Room - 10 rooms");
+        System.out.println("2. Deluxe Room - 5 rooms");
+        System.out.println("3. Suite Room - 2 rooms");
+
+    }
+
 }
