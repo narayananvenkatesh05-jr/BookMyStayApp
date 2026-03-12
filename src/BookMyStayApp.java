cd75ea33a5032aefc44a0d0916f9c568d46f1ca7
narayananvenkatesh05-jr <narayananvenkatesh05@gmail.com>
2026-03-12 09:22:20 +0530

    UC1: Application entry and welcome message

--- src/BookMyStayApp.java
+++ src/BookMyStayApp.java
@@ -1,2 +1,11 @@
 public class BookMyStayApp {
+
+    public static void main(String[] args) {
+
+        System.out.println("=================================");
+        System.out.println("     WELCOME TO BOOK MY STAY     ");
+        System.out.println("=================================");
+        System.out.println("Hotel Booking System Started");
+
+    }
 }
