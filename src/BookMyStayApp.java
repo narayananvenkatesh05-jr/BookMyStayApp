09d14773a861bddad9133774223305bcef3b2d09
narayananvenkatesh05-jr <narayananvenkatesh05@gmail.com>
2026-03-12 09:45:07 +0530

    UC5: Booking request module

--- src/BookMyStayApp.java
+++ src/BookMyStayApp.java
@@ -1,40 +1,17 @@
 public class BookMyStayApp {
 
-    static int standardRooms = 10;
-    static int deluxeRooms = 5;
-    static int suiteRooms = 2;
-
     public static void main(String[] args) {
 
-        checkAvailability("Standard");
-        checkAvailability("Deluxe");
-        checkAvailability("Suite");
-        checkAvailability("Premium");
+        bookingRequest("Standard");
+        bookingRequest("Deluxe");
+        bookingRequest("Suite");
 
     }
 
-    public static void checkAvailability(String roomType) {
+    public static void bookingRequest(String roomType) {
 
-        if (roomType.equalsIgnoreCase("Standard")) {
-
-            System.out.println("Standard Rooms Available: " + standardRooms);
-
-        }
-        else if (roomType.equalsIgnoreCase("Deluxe")) {
-
-            System.out.println("Deluxe Rooms Available: " + deluxeRooms);
-
-        }
-        else if (roomType.equalsIgnoreCase("Suite")) {
-
-            System.out.println("Suite Rooms Available: " + suiteRooms);
-
-        }
-        else {
-
-            System.out.println("Invalid Room Type");
-
-        }
+        System.out.println("Booking request received for " + roomType + " room");
 
     }
+
 }
