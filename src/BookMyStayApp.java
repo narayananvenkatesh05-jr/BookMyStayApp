1ab06ed17c0319610cb6e58d31b5a3d080bb037f
narayananvenkatesh05-jr <narayananvenkatesh05@gmail.com>
2026-03-12 09:47:32 +0530

    UC6: Reservation confirmation and room allocation

--- src/BookMyStayApp.java
+++ src/BookMyStayApp.java
@@ -1,16 +1,43 @@
 public class BookMyStayApp {
 
+    static int standardRooms = 10;
+    static int deluxeRooms = 5;
+    static int suiteRooms = 2;
+
     public static void main(String[] args) {
 
-        bookingRequest("Standard");
-        bookingRequest("Deluxe");
-        bookingRequest("Suite");
+        confirmBooking("Standard");
+        confirmBooking("Deluxe");
+        confirmBooking("Suite");
+        confirmBooking("Suite");
 
     }
 
-    public static void bookingRequest(String roomType) {
+    public static void confirmBooking(String roomType) {
 
-        System.out.println("Booking request received for " + roomType + " room");
+        if (roomType.equalsIgnoreCase("Standard") && standardRooms > 0) {
+
+            standardRooms--;
+            System.out.println("Standard Room Booked");
+
+        }
+        else if (roomType.equalsIgnoreCase("Deluxe") && deluxeRooms > 0) {
+
+            deluxeRooms--;
+            System.out.println("Deluxe Room Booked");
+
+        }
+        else if (roomType.equalsIgnoreCase("Suite") && suiteRooms > 0) {
+
+            suiteRooms--;
+            System.out.println("Suite Room Booked");
+
+        }
+        else {
+
+            System.out.println("Room Not Available");
+
+        }
 
     }
 
